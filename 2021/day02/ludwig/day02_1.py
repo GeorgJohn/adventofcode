3c3a9d76abf94df0ce403e32b5dd538a721fa07d
Ludwig John <ludwigjohn@gmx.de>
2021-12-02 20:17:20 +0100

    solution day 2

--- 2021/day02/ludwig/day02_1.py
+++ 2021/day02/ludwig/day02_1.py
@@ -2,4 +2,17 @@
 # -*- coding: utf-8 -*-
 
 with open("input_1.txt", 'r') as fh:
-	pass
+    pos = 0
+    depth = 0
+    for line in fh:
+        if line.startswith('forward'):
+            pos += int(line.split(' ')[1])
+        elif line.startswith('down'):
+            depth += int(line.split(' ')[1])
+        elif line.startswith('up'):
+            depth -= int(line.split(' ')[1])
+        else:
+            print('error')
+        print(line, pos, depth)
+
+    print(pos * depth)
--- 2021/day02/ludwig/day02_2.py
+++ 2021/day02/ludwig/day02_2.py
@@ -2,4 +2,20 @@
 # -*- coding: utf-8 -*-
 
 with open("input_2.txt", 'r') as fh:
-	pass
+    pos = 0
+    depth = 0
+    aim = 0
+    for line in fh:
+        value = int(line.split(' ')[1])
+        if line.startswith('forward'):
+            pos += value
+            depth += aim * value
+        elif line.startswith('down'):
+            aim += value
+        elif line.startswith('up'):
+            aim -= value
+        else:
+            print('error')
+        print(line, pos, depth, aim)
+
+    print(pos * depth)
