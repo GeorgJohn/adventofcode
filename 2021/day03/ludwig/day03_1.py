3bac1190270f81da8b1a782c72ed92c340ffa830
Ludwig John <ludwigjohn@gmx.de>
2021-12-03 07:33:52 +0100

    solution day 3

--- 2021/day03/ludwig/day03_1.py
+++ 2021/day03/ludwig/day03_1.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+one_count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+zero_count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
 with open("input.txt", 'r') as fh:
-    pass
+    for line in fh:
+        for i in range(0, 12):
+            if line[i] == '1':
+                one_count[i] += 1
+            else:
+                zero_count[i] += 1
+
+    gamma_str = ''
+    epsilon_str = ''
+
+    for i in range(0, 12):
+        if one_count[i] >= zero_count[i]:
+            gamma_str += '1'
+            epsilon_str += '0'
+        else:
+            gamma_str += '0'
+            epsilon_str += '1'
+
+    print(int(gamma_str, 2) * int(epsilon_str, 2))
--- 2021/day03/ludwig/day03_2.py
+++ 2021/day03/ludwig/day03_2.py
@@ -1,5 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+def most_common_bit_1(pos, numbers):
+    one_count = 0
+    zero_count = 0
+    for n in numbers:
+        if (n & (1 << pos)) != 0:
+            one_count += 1
+        else:
+            zero_count += 1
+
+    return one_count >= zero_count
+
+
 with open("input.txt", 'r') as fh:
-    pass
+    numbers = []
+
+    for line in fh:
+        numbers.append(int(line, 2))
+
+    oxygen = numbers
+    CO2 = numbers
+    for pos in range(11, -1, -1):
+        if len(oxygen) != 1:
+            if most_common_bit_1(pos, oxygen):
+                f = filter(lambda x: (x & (1 << pos)) != 0, oxygen)
+                oxygen = list(f)
+            else:
+                f = filter(lambda x: (x & (1 << pos)) == 0, oxygen)
+                oxygen = list(f)
+        if len(CO2) != 1:
+            if most_common_bit_1(pos, CO2):
+                f = filter(lambda x: (x & (1 << pos)) == 0, CO2)
+                CO2 = list(f)
+            else:
+                f = filter(lambda x: (x & (1 << pos)) != 0, CO2)
+                CO2 = list(f)
+
+    print(oxygen[0] * CO2[0])
