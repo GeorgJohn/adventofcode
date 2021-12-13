79bc2b93e3d3881e425866f6858893ae4736df58
Ludwig John <ludwigjohn@gmx.de>
2021-12-13 07:24:54 +0100

    solution day 13

---
+++ 2021/day13/ludwig/a.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import numpy as np
+
+np.set_printoptions(threshold=sys.maxsize)
+paper = np.zeros((1311, 895))
+
+fold_instructions = []
+
+
+def fold_along_x(paper, pos):
+    x_max = paper.shape[0]
+    for i, j in zip(range(pos+1, x_max), range(pos-1, -1, -1)):
+        paper[j] += paper[i]
+
+    return paper[0:pos]
+
+
+def fold_along_y(paper, pos):
+    y_max = paper.shape[1]
+    for i, j in zip(range(pos+1, y_max), range(pos-1, -1, -1)):
+        paper[:,j] += paper[:,i]
+
+    return paper[:,0:pos]
+
+
+with open("input.txt", 'r') as fh:
+    read_folds = False
+    for line in fh:
+        if read_folds:
+            inst = line.split(' ')[2]
+            direction = inst.split('=')[0]
+            position = int(inst.split('=')[1][:-1])
+            fold_instructions.append((direction, position))
+        else:
+            if line != '\n':
+                x = int(line.split(',')[0])
+                y = int(line.split(',')[1][:-1])
+                paper[x][y] = 1
+            else:
+                read_folds = True
+
+    paper = fold_along_x(paper, fold_instructions[0][1])
+    print(len(np.argwhere(paper>0)))
+
